446fb0398b4642eae71e8fe10b3d729023fd00eb
Ricardo Texeira <ricardodeveloper@outlook.com>
2024-03-17 22:22:26 -0300

    chore: criando toda a interface pt 1

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,156 @@
+export function IconExclamationTriangle() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
+      />
+    </svg>
+  );
+}
+
+export function IconChevronUp() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m4.5 15.75 7.5-7.5 7.5 7.5"
+      />
+    </svg>
+  );
+}
+
+export function IconShoppingBag({ width = 6 }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={`w-${width} h-${width}`}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+      />
+    </svg>
+  );
+}
+
+export function IconChatBubbleLeft() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+      />
+    </svg>
+  );
+}
+
+export function IconCogTooth() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+      />
+    </svg>
+  );
+}
+
+export function IconCreditCard() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-4 h-4"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z"
+      />
+    </svg>
+  );
+}
+
+export function IconAcademicCap() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+      />
+    </svg>
+  );
+}
+
+export function IconChevronRight() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m8.25 4.5 7.5 7.5-7.5 7.5"
+      />
+    </svg>
+  );
+}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,277 @@
+import {
+  IconChevronRight,
+  IconChevronUp,
+  IconCreditCard,
+  IconExclamationTriangle,
+  IconShoppingBag,
+} from "@/components/icons";
+
 export default function App() {
-   return (
-      <>
-         <h1 className=""></h1>
-      </>
-   );
+  const filters = [
+    "fatura",
+    "confirmados",
+    "limite",
+    "negados",
+    "contestados",
+    "com bloqueio (referidos)",
+    "em análise",
+    "pendentes",
+    "cancelados",
+    "rejeitados",
+  ];
+
+  return (
+    <>
+      <div className=" container pt-20 bg-zinc-50">
+        <h1 className=" text-2xl font-medium">Linha do tempo de lançamentos</h1>
+
+        <div className="flex my-4 items-center">
+          <h4 className="text-sm">Filtrar exibição:</h4>
+
+          {filters.map((filter, index) => (
+            <div
+              key={index}
+              className=" border border-rose-500 hover:bg-rose-500 hover:cursor-pointer rounded-sm flex justify-center mx-3 p-1"
+            >
+              <p className="text-sm hover:text-white text-rose-500">{filter}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="grid grid-cols-2 mt-10">
+          <div>
+            <div className="flex justify-between my-3 items-center border-b pb-2 mb-6">
+              <div className="flex">
+                <IconExclamationTriangle />
+                <p className="text-sm pl-3">Lançamentos críticos</p>
+              </div>
+              <div className="flex">
+                <div className="rounded-full bg-fuchsia-800 py-0 px-2 mr-2">
+                  <p className=" text-white rounded-full">2</p>
+                </div>
+                <IconChevronUp />
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <p className="text-sm mb-3">Bloqueio de cartão (referido)</p>
+              <p className="text-xs">04 de abril de 2023</p>
+
+              <div className="flex items-center justify-between h-20 hover:bg-zinc-200 hover:cursor-pointer">
+                <div className="flex items-center">
+                  <IconShoppingBag />
+                  <div className=" pl-3">
+                    <p className=" text-sm font-medium">
+                      [Negado] Mercado Livre
+                    </p>
+                    <p className="text-sm">Final 1234</p>
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <div className="pr-3">
+                    <p className="text-sm">R$ 1.000,00</p>
+                    <p className="text-sm text-right">Em 3x</p>
+                  </div>
+                  <IconChevronRight />
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col">
+              <p className="text-sm mb-3">
+                Requer confirmação de cliente (suspeito)
+              </p>
+              <p className="text-xs">04 de abril de 2023</p>
+
+              <div className="flex items-center justify-between h-20 hover:bg-zinc-200 hover:cursor-pointer">
+                <div className="flex items-center">
+                  <IconShoppingBag />
+                  <div className=" pl-3">
+                    <p className=" text-sm font-medium">Renner</p>
+                    <p className="text-sm">Adicional 5678 | Cartão Virtual</p>
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <div className="pr-3">
+                    <p className="text-sm">R$ 130,00</p>
+                    <p className="text-sm text-right">Em 12x</p>
+                  </div>
+                  <IconChevronRight />
+                </div>
+              </div>
+            </div>
+
+            <div className=" mt-10 border p-2 rounded-2xl">
+              <h4 className="text-lg my-3 font-medium">Demais lançamentos</h4>
+
+              <div>
+                <p className="text-xs">04 de abril de 2023</p>
+
+                <div className="flex items-center justify-between h-20 hover:bg-zinc-200 hover:cursor-pointer">
+                  <div className="flex items-center">
+                    <IconShoppingBag />
+                    <div className=" pl-3">
+                      <p className=" text-sm font-medium">
+                        [Negado] Mercado Livre
+                      </p>
+                      <p className="text-sm">Final 1234</p>
+                    </div>
+                  </div>
+                  <div className="flex items-center">
+                    <div className="pr-3">
+                      <p className="text-sm">R$ 1.000,00</p>
+                      <p className="text-sm text-right">Em 3x</p>
+                    </div>
+                    <IconChevronRight />
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-between h-20 hover:bg-zinc-200 hover:cursor-pointer">
+                  <div className="flex items-center">
+                    <IconShoppingBag />
+                    <div className=" pl-3">
+                      <p className=" text-sm font-medium">
+                        [Negado] Mercado Livre
+                      </p>
+                      <p className="text-sm">Final 1234</p>
+                    </div>
+                  </div>
+                  <div className="flex items-center">
+                    <div className="pr-3">
+                      <p className="text-sm">R$ 1.000,00</p>
+                      <p className="text-sm text-right">Em 3x</p>
+                    </div>
+                    <IconChevronRight />
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-between h-20 hover:bg-zinc-200 hover:cursor-pointer">
+                  <div className="flex items-center">
+                    <IconShoppingBag />
+                    <div className=" pl-3">
+                      <p className=" text-sm font-medium">
+                        [Negado] Mercado Livre
+                      </p>
+                      <p className="text-sm">Final 1234</p>
+                    </div>
+                  </div>
+                  <div className="flex items-center">
+                    <div className="pr-3">
+                      <p className="text-sm">R$ 1.000,00</p>
+                      <p className="text-sm text-right">Em 3x</p>
+                    </div>
+                    <IconChevronRight />
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-between h-20 hover:bg-zinc-200 hover:cursor-pointer">
+                  <div className="flex items-center">
+                    <IconShoppingBag />
+                    <div className=" pl-3">
+                      <p className=" text-sm font-medium">
+                        [Negado] Mercado Livre
+                      </p>
+                      <p className="text-sm">Final 1234</p>
+                    </div>
+                  </div>
+                  <div className="flex items-center">
+                    <div className="pr-3">
+                      <p className="text-sm">R$ 1.000,00</p>
+                      <p className="text-sm text-right">Em 3x</p>
+                    </div>
+                    <IconChevronRight />
+                  </div>
+                </div>
+
+                <p className="text-xs">04 de abril de 2023</p>
+                <div className="flex items-center justify-between h-20 hover:bg-zinc-200 hover:cursor-pointer">
+                  <div className="flex items-center">
+                    <IconShoppingBag />
+                    <div className=" pl-3">
+                      <p className=" text-sm font-medium">Renner</p>
+                      <p className="text-sm">Adicional 5678 | Cartão Virtual</p>
+                    </div>
+                  </div>
+                  <div className="flex items-center">
+                    <div className="pr-3">
+                      <p className="text-sm">R$ 130,00</p>
+                      <p className="text-sm text-right">Em 12x</p>
+                    </div>
+                    <IconChevronRight />
+                  </div>
+                </div>
+                <div className="flex items-center justify-between h-20 hover:bg-zinc-200 hover:cursor-pointer">
+                  <div className="flex items-center">
+                    <IconShoppingBag />
+                    <div className=" pl-3">
+                      <p className=" text-sm font-medium">Renner</p>
+                      <p className="text-sm">Adicional 5678 | Cartão Virtual</p>
+                    </div>
+                  </div>
+                  <div className="flex items-center">
+                    <div className="pr-3">
+                      <p className="text-sm">R$ 130,00</p>
+                      <p className="text-sm text-right">Em 12x</p>
+                    </div>
+                    <IconChevronRight />
+                  </div>
+                </div>
+                <div className="flex items-center justify-between h-20 hover:bg-zinc-200 hover:cursor-pointer">
+                  <div className="flex items-center">
+                    <IconShoppingBag />
+                    <div className=" pl-3">
+                      <p className=" text-sm font-medium">Renner</p>
+                      <p className="text-sm">Adicional 5678 | Cartão Virtual</p>
+                    </div>
+                  </div>
+                  <div className="flex items-center">
+                    <div className="pr-3">
+                      <p className="text-sm">R$ 130,00</p>
+                      <p className="text-sm text-right">Em 12x</p>
+                    </div>
+                    <IconChevronRight />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className=" bg-white h-72 mx-5 rounded-xl p-5">
+              <h4 className=" text-md">Detalhes do lançamento</h4>
+              <div className="flex justify-between mt-3">
+                <p className=" text-xs font-light">
+                  05 de maio de 2022 ás 22:42
+                </p>
+                <div className="flex">
+                  <IconShoppingBag width={4} />
+                  <p className=" text-xs font-light pl-3">Compras</p>
+                </div>
+              </div>
+              <h4 className=" text-lg font-bold pt-1">
+                [Negado] Mercado Livre
+              </h4>
+              <p className=" text-lg py-1">R$ 940,50</p>
+
+              <div className="flex justify-between">
+                <p className=" text-xs font-light">Em até 3x</p>
+                <div className="flex">
+                  <IconCreditCard />
+                  <p className=" text-xs font-light pl-3">Final 1234</p>
+                </div>
+              </div>
+              <div className="flex">
+                <div className=" bg-red-600 mr-3 rounded-sm">
+                  <p className="text-white text-xs p-1 ">
+                    Bloqueio (referidas)
+                  </p>
+                </div>
+                <div className=" border p-1 rounded-sm">
+                  <p className="text-xs">Cartão virtual</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
